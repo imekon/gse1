aa1126aa1693f8a4ca8d0972c92bcf732967954f
Pete Goodwin <imekon@users.noreply.github.com>
2019-08-15 18:22:46 +0100

    added initial GSE

---
+++ main.pas
@@ -0,0 +1,26 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMainForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
